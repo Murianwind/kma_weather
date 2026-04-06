a79eef359f408a334569959d2b5684cb91e02302
Murian <y2k001hj@gmail.com>
2026-04-06 10:59:27 +0900

    Update config_flow.py

--- custom_components/kma_weather/config_flow.py
+++ custom_components/kma_weather/config_flow.py
@@ -4,14 +4,13 @@ from homeassistant import config_entries
 from homeassistant.helpers import selector
 from .const import DOMAIN, CONF_API_KEY, CONF_LOCATION_ENTITY
 
-class KMAConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
+class KMAWeatherConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     """Handle a config flow for KMA Weather."""
     VERSION = 1
 
     async def async_step_user(self, user_input=None):
         """Handle the initial step."""
         if user_input is not None:
-            # 엔티티 ID에서 이름을 추출하여 타이틀 생성
             entity_id = user_input[CONF_LOCATION_ENTITY]
             state = self.hass.states.get(entity_id)
             name = state.name if state else entity_id.split('.')[-1]
@@ -23,7 +22,6 @@ class KMAConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
             step_id="user",
             data_schema=vol.Schema({
                 vol.Required(CONF_API_KEY): str,
-                # 모든 Zone과 Device Tracker를 하나의 목록으로 제공
                 vol.Required(CONF_LOCATION_ENTITY): selector.EntitySelector(
                     selector.EntitySelectorConfig(
                         domain=["zone", "device_tracker"],
