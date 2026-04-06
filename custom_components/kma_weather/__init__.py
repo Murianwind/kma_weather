715a591e83313ee37eafd826832e0b3f8b38cca9
Murian <y2k001hj@gmail.com>
2026-04-06 13:44:13 +0900

    Update __init__.py

--- custom_components/kma_weather/__init__.py
+++ custom_components/kma_weather/__init__.py
@@ -1,25 +1,19 @@
-"""The KMA Weather integration."""
+"""Initialize the KMA Weather integration."""
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
-from homeassistant.helpers.aiohttp_client import async_get_clientsession
+from .const import DOMAIN
+from .coordinator import KMAWeatherUpdateCoordinator
 
-from .api_kma import KMAApiClient
-from .const import DOMAIN, CONF_API_KEY
-from .coordinator import KMADataUpdateCoordinator
-
-PLATFORMS = ["sensor", "weather"]
+# [수정] "button" 플랫폼 추가
+PLATFORMS = ["sensor", "weather", "button"]
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Set up KMA Weather from a config entry."""
-    api_key = entry.data[CONF_API_KEY]
-    session = async_get_clientsession(hass)
-    
-    api = KMAApiClient(api_key, session)
-    coordinator = KMADataUpdateCoordinator(hass, api, entry)
-    
+    hass.data.setdefault(DOMAIN, {})
+    coordinator = KMAWeatherUpdateCoordinator(hass, entry)
     await coordinator.async_config_entry_first_refresh()
     
-    hass.data.setdefault(DOMAIN, {})[entry.entry_id] = coordinator
+    hass.data[DOMAIN][entry.entry_id] = coordinator
     await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
     
     return True
