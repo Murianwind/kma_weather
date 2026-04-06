609eda76e1fcfb18f110604053c6d591c3b9ecea
Murian <y2k001hj@gmail.com>
2026-04-06 10:39:52 +0900

    Update config_flow.py

--- custom_components/kma_weather/config_flow.py
+++ custom_components/kma_weather/config_flow.py
@@ -1,27 +1,30 @@
+"""Config flow for KMA Weather."""
 import voluptuous as vol
 from homeassistant import config_entries
 from homeassistant.helpers import selector
 from .const import *
 
-class KMAConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
+class KMAWeatherConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
+    """Handle a config flow for KMA Weather."""
     VERSION = 1
 
     async def async_step_user(self, user_input=None):
+        """Handle the initial step."""
         if user_input is not None:
-            # 선택된 엔티티에 따라 타이틀 결정
             title = "기상청 날씨"
             if user_input[CONF_LOCATION_TYPE] == LOCATION_TYPE_ZONE:
-                title = f"날씨: {user_input.get(CONF_ZONE_ID).split('.')[-1]}"
+                if user_input.get(CONF_ZONE_ID):
+                    title = f"날씨: {user_input[CONF_ZONE_ID].split('.')[-1]}"
             else:
-                title = f"이동형 날씨: {user_input.get(CONF_MOBILE_DEVICE_ID).split('.')[-1]}"
+                if user_input.get(CONF_MOBILE_DEVICE_ID):
+                    title = f"이동형 날씨: {user_input[CONF_MOBILE_DEVICE_ID].split('.')[-1]}"
             
             return self.async_create_entry(title=title, data=user_input)
 
         return self.async_show_form(
             step_id="user",
             data_schema=vol.Schema({
-                vol.Required(CONF_KMA_API_KEY): str,
-                vol.Required(CONF_AIR_API_KEY): str,
+                vol.Required(CONF_API_KEY): str,
                 vol.Required(CONF_LOCATION_TYPE, default=LOCATION_TYPE_ZONE): selector.SelectSelector(
                     selector.SelectSelectorConfig(
                         options=[
@@ -30,7 +33,6 @@ class KMAConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
                         ], mode=selector.SelectSelectorMode.DROPDOWN
                     )
                 ),
-                # 사용자가 선택한 타입에 맞는 엔티티만 입력하도록 유도
                 vol.Optional(CONF_ZONE_ID): selector.EntitySelector(
                     selector.EntitySelectorConfig(domain="zone")
                 ),
