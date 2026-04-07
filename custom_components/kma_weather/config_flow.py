5008b25dd2f4af58010d5033f81dbd49db34e518
Murian <y2k001hj@gmail.com>
2026-04-07 23:42:03 +0900

    Update config_flow.py

--- custom_components/kma_weather/config_flow.py
+++ custom_components/kma_weather/config_flow.py
@@ -1,29 +1,49 @@
-"""Config flow for KMA Weather."""
 import voluptuous as vol
 from homeassistant import config_entries
+from homeassistant.core import callback
 from homeassistant.helpers import selector
-from .const import DOMAIN, CONF_API_KEY, CONF_LOCATION_ENTITY, CONF_PREFIX
+import homeassistant.helpers.config_validation as cv
+from .const import (
+    DOMAIN, CONF_API_KEY, CONF_NAME, CONF_LOCATION_ENTITY, 
+    CONF_PREFIX, CONF_EXPIRE_DATE, CONF_APPLY_DATE
+)
 
-class KMAConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
+class KMAWeatherConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     VERSION = 1
-
     async def async_step_user(self, user_input=None):
         if user_input is not None:
-            entity_id = user_input[CONF_LOCATION_ENTITY]
-            state = self.hass.states.get(entity_id)
-            # 위치 엔티티의 이름이나 ID의 마지막 부분을 가져와 기기 이름을 결정합니다.
-            name = state.name if state else entity_id.split('.')[-1]
-            # [복구] 기기 이름을 '기상청 날씨: {name}' 형식으로 설정합니다.
-            return self.async_create_entry(title=f"기상청 날씨: {name}", data=user_input)
+            await self.async_set_unique_id(f"{user_input[CONF_API_KEY]}_{user_input[CONF_PREFIX]}")
+            self._abort_if_unique_id_configured()
+            # title에 사용자가 입력한 기기 이름을 사용합니다.
+            return self.async_create_entry(title=user_input[CONF_NAME], data=user_input)
 
         return self.async_show_form(
             step_id="user",
             data_schema=vol.Schema({
                 vol.Required(CONF_API_KEY): str,
-                vol.Required(CONF_LOCATION_ENTITY): selector.EntitySelector(
-                    selector.EntitySelectorConfig(domain=["zone", "device_tracker"])
-                ),
-                # 기본값(default)을 제거하여 완전한 빈칸으로 시작되도록 유지합니다.
+                vol.Required(CONF_NAME): str,
                 vol.Required(CONF_PREFIX): str,
+                vol.Optional(CONF_LOCATION_ENTITY): selector.EntitySelector(selector.EntitySelectorConfig(domain=["zone", "device_tracker", "person"])),
+                vol.Optional(CONF_APPLY_DATE): cv.string,
+                vol.Optional(CONF_EXPIRE_DATE): cv.string,
+            }),
+            description_placeholders={"api_link": "https://www.data.go.kr/"}
+        )
+
+    @staticmethod
+    @callback
+    def async_get_options_flow(config_entry):
+        return KMAWeatherOptionsFlowHandler(config_entry)
+
+class KMAWeatherOptionsFlowHandler(config_entries.OptionsFlow):
+    def __init__(self, config_entry): self._config_entry = config_entry
+    async def async_step_init(self, user_input=None):
+        if user_input is not None: return self.async_create_entry(title="", data=user_input)
+        return self.async_show_form(
+            step_id="init",
+            data_schema=vol.Schema({
+                vol.Optional(CONF_LOCATION_ENTITY, default=self._config_entry.options.get(CONF_LOCATION_ENTITY, self._config_entry.data.get(CONF_LOCATION_ENTITY, ""))): selector.EntitySelector(selector.EntitySelectorConfig(domain=["zone", "device_tracker", "person"])),
+                vol.Optional(CONF_APPLY_DATE, default=self._config_entry.options.get(CONF_APPLY_DATE, self._config_entry.data.get(CONF_APPLY_DATE, ""))): cv.string,
+                vol.Optional(CONF_EXPIRE_DATE, default=self._config_entry.options.get(CONF_EXPIRE_DATE, self._config_entry.data.get(CONF_EXPIRE_DATE, ""))): cv.string,
             })
         )
