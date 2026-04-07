cd617c6a65324a552c7860c12bb713f10b08808c
Murian <y2k001hj@gmail.com>
2026-04-07 23:23:17 +0900

    Update config_flow.py

--- custom_components/kma_weather/config_flow.py
+++ custom_components/kma_weather/config_flow.py
@@ -3,38 +3,25 @@ from homeassistant import config_entries
 from homeassistant.core import callback
 from homeassistant.helpers import selector
 import homeassistant.helpers.config_validation as cv
-from .const import (
-    DOMAIN, CONF_API_KEY, CONF_LOCATION_ENTITY, CONF_PREFIX, 
-    CONF_EXPIRE_DATE, CONF_APPLY_DATE
-)
+from .const import DOMAIN, CONF_API_KEY, CONF_LOCATION_ENTITY, CONF_PREFIX, CONF_EXPIRE_DATE, CONF_APPLY_DATE
 
 class KMAWeatherConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
-    """KMA Weather 설정 흐름."""
     VERSION = 1
-
     async def async_step_user(self, user_input=None):
-        errors = {}
         if user_input is not None:
             await self.async_set_unique_id(f"{user_input[CONF_API_KEY]}_{user_input[CONF_PREFIX]}")
             self._abort_if_unique_id_configured()
             return self.async_create_entry(title=user_input[CONF_PREFIX], data=user_input)
 
-        # 1 & 2. 등록 시 안내 문구 및 신청일/만료일 필드 복구
-        schema = vol.Schema({
-            vol.Required(CONF_API_KEY): str,
-            vol.Required(CONF_PREFIX): str,
-            vol.Optional(CONF_LOCATION_ENTITY): selector.EntitySelector(
-                selector.EntitySelectorConfig(domain=["zone", "device_tracker", "person"])
-            ),
-            vol.Optional(CONF_APPLY_DATE): cv.string,
-            vol.Optional(CONF_EXPIRE_DATE): cv.string,
-        })
-
         return self.async_show_form(
-            step_id="user", 
-            data_schema=schema, 
-            errors=errors,
-            # strings.json의 description 키를 통해 가이드 문구가 출력됩니다.
+            step_id="user",
+            data_schema=vol.Schema({
+                vol.Required(CONF_API_KEY): str,
+                vol.Required(CONF_PREFIX): str,
+                vol.Optional(CONF_LOCATION_ENTITY): selector.EntitySelector(selector.EntitySelectorConfig(domain=["zone", "device_tracker", "person"])),
+                vol.Optional(CONF_APPLY_DATE): cv.string,
+                vol.Optional(CONF_EXPIRE_DATE): cv.string,
+            }),
             description_placeholders={"api_link": "https://www.data.go.kr/"}
         )
 
@@ -43,41 +30,15 @@ class KMAWeatherConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     def async_get_options_flow(config_entry):
         return KMAWeatherOptionsFlowHandler(config_entry)
 
-
 class KMAWeatherOptionsFlowHandler(config_entries.OptionsFlow):
-    """3. 기기 수정에서 신청일/만료일 모두 수정 가능하도록 복구."""
-    def __init__(self, config_entry):
-        self._config_entry = config_entry
-
+    def __init__(self, config_entry): self._config_entry = config_entry
     async def async_step_init(self, user_input=None):
-        if user_input is not None:
-            return self.async_create_entry(title="", data=user_input)
-
-        # 옵션 화면에서도 APPLY_DATE와 EXPIRE_DATE 필드를 모두 유지합니다.
-        options_schema = vol.Schema({
-            vol.Optional(
-                CONF_LOCATION_ENTITY, 
-                default=self._config_entry.options.get(
-                    CONF_LOCATION_ENTITY, 
-                    self._config_entry.data.get(CONF_LOCATION_ENTITY, "")
-                )
-            ): selector.EntitySelector(
-                selector.EntitySelectorConfig(domain=["zone", "device_tracker", "person"])
-            ),
-            vol.Optional(
-                CONF_APPLY_DATE,
-                default=self._config_entry.options.get(
-                    CONF_APPLY_DATE,
-                    self._config_entry.data.get(CONF_APPLY_DATE, "")
-                )
-            ): cv.string,
-            vol.Optional(
-                CONF_EXPIRE_DATE, 
-                default=self._config_entry.options.get(
-                    CONF_EXPIRE_DATE, 
-                    self._config_entry.data.get(CONF_EXPIRE_DATE, "")
-                )
-            ): cv.string,
-        })
-
-        return self.async_show_form(step_id="init", data_schema=options_schema)
+        if user_input is not None: return self.async_create_entry(title="", data=user_input)
+        return self.async_show_form(
+            step_id="init",
+            data_schema=vol.Schema({
+                vol.Optional(CONF_LOCATION_ENTITY, default=self._config_entry.options.get(CONF_LOCATION_ENTITY, self._config_entry.data.get(CONF_LOCATION_ENTITY, ""))): selector.EntitySelector(selector.EntitySelectorConfig(domain=["zone", "device_tracker", "person"])),
+                vol.Optional(CONF_APPLY_DATE, default=self._config_entry.options.get(CONF_APPLY_DATE, self._config_entry.data.get(CONF_APPLY_DATE, ""))): cv.string,
+                vol.Optional(CONF_EXPIRE_DATE, default=self._config_entry.options.get(CONF_EXPIRE_DATE, self._config_entry.data.get(CONF_EXPIRE_DATE, ""))): cv.string,
+            })
+        )
