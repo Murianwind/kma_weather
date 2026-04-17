78809b1829bc92c335e1e30b67be2e44cf8c57b9
Murian <y2k001hj@gmail.com>
2026-04-17 21:32:08 +0900

    Update test_api.py

--- tests/test_api.py
+++ tests/test_api.py
@@ -25,10 +25,9 @@ class MockAiohttpSession:
         return MockAiohttpResponse(json_data=self.json_data, should_raise=self.should_raise)
 
 def test_api_key_decoding():
-    original_key = "test_secret_key!@#"
-    encoded_key = quote(original_key)
+    encoded_key = quote("test_secret_key!@#")
     api = KMAWeatherAPI(None, encoded_key)
-    assert api.api_key == original_key
+    assert api.api_key == "test_secret_key!@#"
 
 @pytest.mark.asyncio
 async def test_fetch_http_error(caplog):
